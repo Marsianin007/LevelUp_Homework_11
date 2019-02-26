433b98ed7d033515563eb2e2f8161ef41bf784d2
Marsianin007 <chernoborodiuk20@gmail.com>
2019-02-26 20:48:28 +0200

    All levels

---
+++ script.js
@@ -0,0 +1,151 @@
+//1
+for(var i=0; i <2; i++){north()};
+
+//2
+for(var i=0; i < 16; i++){east()};
+
+//3
+for(var i=0; i <7; i++){south(); east();};
+
+ //4
+
+    for(var i=0; i < 21; i++){if	(isFree("south"))	
+        {
+           south()
+        } else{
+        east()};	}
+
+//5
+    for(var i=0; i < 6; i++){
+        east();
+    }
+
+    for(var i=0; i < 2; i++){
+        south();
+    }
+
+    for(var i=0; i < 2; i++){
+        west();
+    }
+    for(var i=0; i < 3; i++){
+        south();
+    }
+
+    for(var i=0; i < 2; i++){
+        west();
+    }
+
+    for(var i=0; i < 3; i++){
+        north();
+    }
+    for(var i=0; i < 2; i++){
+        west();
+    }
+
+    for(var i=0; i < 5; i++){
+        south();
+    }
+
+    for(var i=0; i < 6; i++){
+        east();
+    }
+
+//6
+
+    
+    for(var i=0; i < 5; i++){
+        south();
+    }
+
+    for(var i=0; i < 2; i++){
+        east();
+    }
+
+    for(var i=0; i < 3; i++){
+        north();
+    }
+
+
+        east();
+
+
+    for(var i=0; i < 2; i++){
+        north();
+    }
+
+    for(var i=0; i < 3; i++){
+        east();
+    }
+
+    for(var i=0; i < 4; i++){
+        south();
+    }
+
+    for(var i=0; i < 2; i++){
+        west();
+    }
+
+    for(var i=0; i < 3; i++){
+        south();
+    }
+
+    for(var i=0; i < 2; i++){
+        east();
+    }
+
+
+//7
+
+    for(var i=0; i < 11; i++){
+        south();
+    }
+
+    for(var i=0; i < 4; i++){
+        east();
+    }
+
+    for(var i=0; i < 11; i++){
+        north();
+    }
+
+    for(var i=0; i < 2; i++){
+        east();
+    }
+
+    south();
+    east();
+
+    for(var i=0; i < 5; i++){
+        south();
+    }
+
+    west();
+
+    for(var i=0; i < 7; i++){
+        south();
+    }
+
+    for(var i=0; i < 8; i++){
+        east();
+    }
+
+    for(var i=0; i < 9; i++){
+        south();
+    }
+
+    east();
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
